d52b0749fbd61029e8adf73187a148da4336902f
Kyaw Soe Hla <codewarrior.ksh@gmail.com>
2025-12-01 11:16:41 +0630

    aa

--- office/settings/development.py
+++ office/settings/development.py
@@ -3,7 +3,7 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8000"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000", "https://office-management-cj00.onrender.com", "https://office.arakkha.tech"]
 
 DATABASES = {
     'default': {
