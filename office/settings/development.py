6c49f91f87ffdd50faa89e14ba9913658b1c6f38
Kyaw Soe Hla <codewarrior.ksh@gmail.com>
2025-12-05 21:44:05 +0630

    changed db

--- office/settings/development.py
+++ office/settings/development.py
@@ -5,25 +5,25 @@ ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000", "https://office-management-cj00.onrender.com", "https://office.arakkha.tech"]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'defaultdb',
-#         'USER': 'doadmin',
-#         'PASSWORD': 'AVNS_gwogUwAiAF0S5sawMEb',
-#         'HOST': 'office-do-user-22229090-0.h.db.ondigitalocean.com',
-#         'PORT': '25060',
-#         'OPTIONS': {
-#             'ssl_mode': 'REQUIRED',
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'defaultdb',
+        'USER': 'avnadmin',
+        'PASSWORD': 'AVNS_0JEyPxPfwq9Exx_cTn-',
+        'HOST': 'office-kyawsoehla.c.aivencloud.com',
+        'PORT': '25741',
+        'OPTIONS': {
+            'ssl_mode': 'REQUIRED',
+        },
+    }
+}
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_ROOT = BASE_DIR / "media"
--- office/settings/production.py
+++ office/settings/production.py
@@ -13,14 +13,27 @@ CSRF_TRUSTED_ORIGINS = [
     "https://office.arakkha.tech",
 ]
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ.get("DB_NAME", "defaultdb"),
+#         'USER': os.environ.get("DB_USER", "doadmin"),
+#         'PASSWORD': os.environ.get("DB_PASSWORD"),
+#         'HOST': os.environ.get("DB_HOST"),
+#         'PORT': os.environ.get("DB_PORT", "25060"),
+#         'OPTIONS': {
+#             'ssl_mode': 'REQUIRED',
+#         },
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get("DB_NAME", "defaultdb"),
-        'USER': os.environ.get("DB_USER", "doadmin"),
+        'USER': os.environ.get("DB_USER", "avnadmin"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT", "25060"),
+        'PORT': os.environ.get("DB_PORT", "25741"),
         'OPTIONS': {
             'ssl_mode': 'REQUIRED',
         },
