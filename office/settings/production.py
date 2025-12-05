74b2626a019e430f46e678f07781de125b840f35
Kyaw Soe Hla <codewarrior.ksh@gmail.com>
2025-12-05 22:30:08 +0630

    add change

--- office/settings/production.py
+++ office/settings/production.py
@@ -13,32 +13,32 @@ CSRF_TRUSTED_ORIGINS = [
     "https://office.arakkha.tech",
 ]
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.environ.get("DB_NAME", "defaultdb"),
-#         'USER': os.environ.get("DB_USER", "doadmin"),
-#         'PASSWORD': os.environ.get("DB_PASSWORD"),
-#         'HOST': os.environ.get("DB_HOST"),
-#         'PORT': os.environ.get("DB_PORT", "25060"),
-#         'OPTIONS': {
-#             'ssl_mode': 'REQUIRED',
-#         },
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get("DB_NAME", "defaultdb"),
-        'USER': os.environ.get("DB_USER", "avnadmin"),
+        'USER': os.environ.get("DB_USER", "doadmin"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT", "25741"),
+        'PORT': os.environ.get("DB_PORT", "25060"),
         'OPTIONS': {
             'ssl_mode': 'REQUIRED',
         },
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ.get("DB_NAME", "defaultdb"),
+#         'USER': os.environ.get("DB_USER", "avnadmin"),
+#         'PASSWORD': os.environ.get("DB_PASSWORD"),
+#         'HOST': os.environ.get("DB_HOST"),
+#         'PORT': os.environ.get("DB_PORT", "25741"),
+#         'OPTIONS': {
+#             'ssl_mode': 'REQUIRED',
+#         },
+#     }
+# }
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = BASE_DIR / "media"
