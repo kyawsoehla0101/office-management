28d7c965ab00dad7483ba34a985e1975691deaa4
Kyaw Soe Hla <codewarrior.ksh@gmail.com>
2025-12-23 08:00:10 +0630

    Edited device not allowed content

--- office/settings/development.py
+++ office/settings/development.py
@@ -5,27 +5,27 @@ ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000", "https://office-management-cj00.onrender.com", "https://office.arakkha.tech"]
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get("DB_NAME", "defaultdb"),
-        'USER': os.environ.get("DB_USER", "doadmin"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT", "25060"),
-        'OPTIONS': {
-            'ssl_mode': 'REQUIRED',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'defaultdb',
+#         'USER': 'doadmin',
+#         'PASSWORD': 'AVNS_gwogUwAiAF0S5sawMEb',
+#         'HOST': 'office-do-user-22229090-0.h.db.ondigitalocean.com',
+#         'PORT': '25060',
+#         'OPTIONS': {
+#             'ssl_mode': 'REQUIRED',
+#         },
+#     }
+# }
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_ROOT = BASE_DIR / "media"
